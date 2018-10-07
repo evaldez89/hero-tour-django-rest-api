7e7d840a885f08019321a6ac9da2e2e6dadd6f65
Emmanuel Valdez <evaldez89@gmail.com>
2018-10-06 21:33:15 -0400

    Add hero model

--- heroes/models.py
+++ heroes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Hero(models.Model):
+    name = models.CharField(max_length=50)
+    secret_identity = models.CharField(max_length=50)
+    super_power = models.CharField(max_length=20)
+    strength = models.IntegerField(default=1)
+    inserted_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+
+class Villain(Hero):
+    pass
