89354a58ccef83df03b69707f8d3cd5a92811595
Emmanuel Valdez <evaldez89@gmail.com>
2018-10-07 23:48:51 -0400

    add rest framework and heroes app to settings

--- heroes_api/settings.py
+++ heroes_api/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'heroes',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'heroes',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
